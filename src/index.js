c96ab1c0663a394d02cacc0ac815b4686c1ffccc
Dan Kiss <flowfast47@gmail.com>
2022-07-11 15:27:48 -0700

    add in react-router

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
+import Shop from './shop'
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,45 @@
+import Directory from '../../components/directory/directory.component'
+import { Link, Outlet } from 'react-router-dom';
+
+const App = () => {
+  const categories = [
+    {
+      "id": 1,
+      "title": "hats",
+      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+    },
+    {
+      "id": 2,
+      "title": "jackets",
+      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+    },
+    {
+      "id": 3,
+      "title": "sneakers",
+      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+    },
+    {
+      "id": 4,
+      "title": "womens",
+      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+    },
+    {
+      "id": 5,
+      "title": "mens",
+      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+    }
+  ]
+
+  return (
+    <div>
+      <Outlet />
+      {/* <nav>
+        <Link to="shop">Shop</Link>
+        <Link to="contact">Contact</Link>
+      </nav> */}
+      <Directory categories={categories} />
+    </div>
+  );
+};
+
+export default App;
